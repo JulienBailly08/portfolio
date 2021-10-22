f5a2702472100963f8ceaf6deac075366003bd5e
Julien_B <julienbailly08@gmail.com>
2021-10-22 18:46:37 +0200

    add mail contact fonctionnal

--- js/script.js
+++ js/script.js
@@ -36,95 +36,48 @@ $("h1").hover(
   }
 );
 
+const submitButton = $("#submit");
+const infoMail =$("#infoMail");
+let firstname = $("#firstname");
+let lastname = $("#lastname");
+let mail = $("#mail");
+let text = $("#text");
 
 
 
+submitButton.on("click",function(envoi){
+    envoi.preventDefault();
+    $.ajax({
+        url:"../mail/sendEmail.php",
+        type:"POST",
+        data:{
+            firstname: firstname.val(),
+            lastname: lastname.val(),
+            mail: mail.val(),
+			text: text.val()
+        },
+    }).done(function(OK){
+	
+	
+		if(OK =='ok'){
+		infoMail.text("Message envoyé");
+		setTimeout(function(){
+			infoMail.text("");
+		}, 3000);
 
-(function($) {
+	}
+	else{
+		infoMail.text("Merci de renseigner tout les champs");
+		setTimeout(function(){
+			infoMail.text("");
+		}, 3000);
+	}
+	
+    }).fail(function(erreur){
+        console.log(erreur);
+    });
+});
 
-	"use strict";
-
-
-  // Form
-	var contactForm = function() {
-		if ($('#contactForm').length > 0 ) {
-			$( "#contactForm" ).validate( {
-				rules: {
-					name: "required",
-					subject: "required",
-					email: {
-						required: true,
-						email: true
-					},
-					message: {
-						required: true,
-						minlength: 5
-					}
-				},
-				messages: {
-					name: "Please enter your name",
-					subject: "Please enter your subject",
-					email: "Please enter a valid email address",
-					message: "Please enter a message"
-				},
-				/* submit via ajax */
-				
-				submitHandler: function(form) {		
-					var $submit = $('.submitting'),
-						waitText = 'Submitting...';
-
-					$.ajax({   	
-				      type: "POST",
-				      url: "php/sendEmail.php",
-				      data: $(form).serialize(),
-
-				      beforeSend: function() { 
-				      	$submit.css('display', 'block').text(waitText);
-				      },
-				      success: function(msg) {
-		               if (msg == 'OK') {
-		               	$('#form-message-warning').hide();
-				            setTimeout(function(){
-		               		$('#contactForm').fadeIn();
-		               	}, 1000);
-				            setTimeout(function(){
-				               $('#form-message-success').fadeIn();   
-		               	}, 1400);
-
-		               	setTimeout(function(){
-				               $('#form-message-success').fadeOut();   
-		               	}, 8000);
-
-		               	setTimeout(function(){
-				               $submit.css('display', 'none').text(waitText);  
-		               	}, 1400);
-
-		               	setTimeout(function(){
-		               		$( '#contactForm' ).each(function(){
-											    this.reset();
-											});
-		               	}, 1400);
-			               
-			            } else {
-			               $('#form-message-warning').html(msg);
-				            $('#form-message-warning').fadeIn();
-				            $submit.css('display', 'none');
-			            }
-				      },
-				      error: function() {
-				      	$('#form-message-warning').html("Something went wrong. Please try again.");
-				         $('#form-message-warning').fadeIn();
-				         $submit.css('display', 'none');
-				      }
-			      });    		
-		  		} // end submitHandler
-
-			});
-		}
-	};
-	contactForm();
-
-})(jQuery);
 
 
 
